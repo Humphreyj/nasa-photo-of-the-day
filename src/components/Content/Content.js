423734fb525e7737683818ea313d9ca6db0a947d
HumphreyJ <joshhumphrey1@gmail.com>
2019-10-26 22:16:25 -0500

    improved the styling on desktop.

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -8,9 +8,22 @@ line-height: 2em;
 font-size: 1.3em;
 padding: 0% 1%;
 height: 10em;
+width: 80%;
+margin: 0 auto;
 overflow-y: scroll;
 box-sizing: content-box;
-color: white
+color: white;
+&::-webkit-scrollbar {
+    display: none;
+    }
+    @media(min-width: 700px) {
+        & {
+            width: 80%;
+            font-size: 2em;
+            margin-right: 14%;
+        }
+    }
+
 `
 const Content = (props) => {
     return (
@@ -19,8 +32,9 @@ const Content = (props) => {
                 <h3 className='date' >POTD for {props.date}</h3>
             
             {/* date info */}
+            <h3 className="title">{props.title}</h3>
             <div className="media-container">
-                <h3 className="title">{props.title}</h3>
+                
                 
                 <Media
                 title={props.title}
